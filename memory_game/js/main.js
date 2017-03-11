b8a27507fc5cf2ef7ba578ea3b9d0ce6c60bae7e
n0205693 <rachel.daniels@libertymutual.com>
2017-03-11 11:42:07 -0500

    created game logic unit 8

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
