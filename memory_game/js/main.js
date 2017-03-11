ce2290bb8ecbdf3bba44d1e42f0abcf422a92d60
n0205693 <rachel.daniels@libertymutual.com>
2017-03-11 15:59:04 -0500

    created game logic for unit 10

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 
 var cardsInPlay = [];
 
@@ -11,14 +32,18 @@ var checkForMatch = function (){
 };
 
 var flipCard = function (cardId1, cardId2) {
-	console.log("User flipped " + cards[cardId1]);
-	cardsInPlay.push(cards[cardId1]);
-	console.log("User flipped " + cards[cardId2]);
-	cardsInPlay.push(cards[cardId2]);
+	console.log("User flipped " + cards[cardId1].rank);
+	console.log(cards[cardId1].cardImage);
+	console.log(cards[cardId1].suit);
+	cardsInPlay.push(cards[cardId1].rank);
+	console.log("User flipped " + cards[cardId2].rank);
+	console.log(cards[cardId2].cardImage);
+	console.log(cards[cardId2].suit);
+	cardsInPlay.push(cards[cardId2].rank);
 	checkForMatch();
 };
 
-flipCard(0, 2);
+flipCard (0, 2);
 
 
 
